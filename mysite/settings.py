734185dff029c4be102ef3f89c5a6e7f01309910
Mike <munkyu0813@gmail.com>
2017-08-20 13:59:50 -0400

    add share by email feature and outline schema for comments

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,3 +103,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Email settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'munkyu0813@gmail.com'
+EMAIL_HOST_PASSWORD = 'gkr95jgkr95j'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
