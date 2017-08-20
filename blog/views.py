78b062a5aa6dd81d79c4b9d4d15a4e84566c3d7c
Mike <munkyu0813@gmail.com>
2017-08-20 15:35:55 -0400

    display related posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
@@ -67,11 +68,19 @@ def post_detail(request, year, month, day, post):
     else:
         # GET request; display empty form
         comment_form = CommentForm()
-        
+    
+    # List of similar posts
+    post_tag_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tag_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by(
+        '-same_tags', '-publish')[:4]
+
     context = {
         'post': post,
         'comments': comments,
         'comment_form': comment_form,
+        'similar_posts': similar_posts,
     }
     return render(request, 'blog/post/detail.html', context)
 
